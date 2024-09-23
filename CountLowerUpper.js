828cf03e37bb44abec6f3e32a835f9b88715c7c1
aashiashfak <aashiashfak@gmail.com>
2024-09-23 17:11:53 +0530

    Count lowercase and uppercase in a string in js completed

--- CountLowerUpper.js
+++ CountLowerUpper.js
@@ -3,5 +3,16 @@ word = "Hello ";
 lower = 0
 upper = 0
 
+word.split('').forEach(element => {
+    if (element === element.toUpperCase() && element !== element.toLowerCase()){
+        upper ++ 
+    }else if (element === element.toLowerCase() && element !== element.toUpperCase()){
+        lower ++
+    }
+});
+
+console.log('upper:',upper);
+console.log('lower:',lower);
+
 
 
